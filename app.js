77714ddd94ee13a2cdfef82d0814f65ddbc8172b
Satvik <Satvikgovale7@gmail.com>
2024-05-21 17:40:54 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+        // else {
+        //     entry.target.classList.remove('show');
+        //   }
+    })
+})
+
+const hiddenElements = document.querySelectorAll(".hide");
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+function scrollToTop() {
+    window.scrollTo(0, 0);
+}
+
+window.onload = scrollToTop;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('.nav a');
+
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            var targetId = this.getAttribute('href');
+
+            // Scroll to the target section
+            document.querySelector(targetId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    var navToggle = document.querySelector('.nav-toggle');
+    var nav = document.querySelector('.nav');
+
+    navToggle.addEventListener('click', function() {
+        nav.classList.toggle('nav-active');
+    });
+
+});

560c8148c0a908aa0e45a34e82b373fd15a27e47
Satvik <Satvikgovale7@gmail.com>
2024-05-21 18:29:38 +0530

    Header change

--- app.js
+++ app.js
@@ -45,3 +45,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+let subMenu = document.querySelector(".sub-menu");
+
+window.addEventListener("click", (e)=>{
+    if(e.target.closest(".toggle")){
+        subMenu.style.display = "flex";
+    } else {
+        subMenu.style.display = "none";
+    }
+})
+
